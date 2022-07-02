1a08fc5acd9cabbb5b2e8bad3874e6aabb15a032
hasan-ullah <mdhasanullah124@gmail.com>
2022-07-02 06:27:53 +0600

    go arr done

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
-func main()  {
+func myArr() {
+
+	var arr1 = [3]int{1, 2, 3}
+
+	fmt.Println(arr1)
+}
+
+func main() {
+	myArr()
 	fmt.Println("ami bangla valobashi")
+
 }
